6377a8e457ae436da7e1fbaac54df9ef8ca7c0ed
Jo <bettaque@gmail.com>
2024-10-05 11:53:34 +0200

    Initial commit

---
+++ conf.lua
@@ -0,0 +1,14 @@
+function love.conf(t)
+    -- make randomness actually random by using the current time as seed
+    math.randomseed(os.time())
+
+    t.window.title = "LDJAM56"
+
+    -- configure love
+    t.version = "11.5"
+    t.identity = "LDJAM56"
+    t.window.resizable = true
+    t.modules.joystick = false
+    t.modules.touch = false
+    t.console = true
+end
---
+++ main.lua
@@ -0,0 +1,11 @@
+function love.load()
+    print("running on " .. love.system.getOS())
+end
+
+function love.update()
+
+end
+
+function love.draw()
+
+end
