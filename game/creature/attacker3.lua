9dcfb1e963a61898bf3a635bd2f23505baf42ec2
Sharkooon <zweitemage@web.de>
2024-10-06 15:25:29 +0200

    more details

--- game/creature/attacker3.lua
+++ game/creature/attacker3.lua
@@ -1,9 +1,9 @@
 game.creature.attacker = {}
 
-game.creature.attacker.health = 100
-game.creature.attacker.meleeDamage = 20
-game.creature.attacker.rangedDamage = 10
-game.creature.attacker.speed = 1
+game.creature.attacker.health = 150
+game.creature.attacker.meleeDamage = 6
+game.creature.attacker.rangedDamage = 2
+game.creature.attacker.speed = 2
 game.creature.attacker.cooldown = 0.3
 game.creature.attacker.range = 0
 
--- game/creature/attacker4.lua
+++ game/creature/attacker4.lua
@@ -1,7 +1,7 @@
 game.creature.attacker = {}
 
 game.creature.attacker.health = 100
-game.creature.attacker.meleeDamage = 1
+game.creature.attacker.meleeDamage = 3
 game.creature.attacker.rangedDamage = 0
 game.creature.attacker.speed = 1
 game.creature.attacker.cooldown = 0.3
