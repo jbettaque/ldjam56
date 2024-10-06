ebb071bca7d672361e3ac67925f2ac7c6c7d5570
IMMISLAPTOP\imman <immanuel.feyler@googlemail.com>
2024-10-06 22:09:59 +0200

    Game-Over-Screen rework

--- game/creature/dog.lua
+++ game/creature/dog.lua
@@ -7,3 +7,18 @@ game.creature.dog.speed = 3
 game.creature.dog.cooldown = 0.3
 game.creature.dog.range = 0
 
+local dogImage = love.graphics.newImage("game/Sprites/Dog.png")
+
+game.creature.dog.draw = function(creature)
+    love.graphics.setColor(1, 1, 1)
+    if creature.damaged and creature.damaged > 0 then
+        love.graphics.setColor(1, 0, 0)
+    end
+    local transform = love.math.newTransform(creature.x, creature.y, 0, 0.25, 0.25, 32, 32)
+    if (creature.player == 1) then
+        transform:scale(-1, 1)
+    end
+
+    love.graphics.draw(dogImage, transform)
+
+end
--- game/creature/horde.lua
+++ game/creature/horde.lua
@@ -7,3 +7,18 @@ game.creature.horde.speed = 2
 game.creature.horde.cooldown = 0.3
 game.creature.horde.range = 0
 
+local hordeImage = love.graphics.newImage("game/Sprites/Coin.png")
+
+game.creature.horde.draw = function(creature)
+    love.graphics.setColor(1, 1, 1)
+    if creature.damaged and creature.damaged > 0 then
+        love.graphics.setColor(1, 0, 0)
+    end
+    local transform = love.math.newTransform(creature.x, creature.y, 0, 0.25, 0.25, 32, 32)
+    if (creature.player == 1) then
+        transform:scale(-1, 1)
+    end
+
+    love.graphics.draw(hordeImage, transform)
+
+end
