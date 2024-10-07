78c3685ef967fd7cdec89540244b68a51ca68068
sheaksadi <sheaksadi123456@gmail.com>
2024-10-07 21:54:35 +0600

    added tutorial

--- main.lua
+++ main.lua
@@ -6,12 +6,13 @@ require("game.towerPlacement")
 require("game.tMenu")
 require("game.manager")
 require("game.enemyAi")
+require("game.tutorial")
 game.powerUps = require("game.powerUps")
 
 local mainMenu = require("game.mainMenu")
 local gameOverScreen = require("game.gameOverScreen")
 
-local currentState = "menu"
+local currentState = "tutorial"
 
 function switchToGame()
     currentState = "game"
@@ -20,15 +21,17 @@ end
 function switchToPause()
     currentState = "pause"
 end
-
+function switchToTutorial()
+    currentState = "tutorial"
+end
 function endGame()
     currentState = "gameover"
 end
 function switchToMainMenu()
     mainMenu.currentMenu = "main"
 end
-screenWidth = 1920
-screenHeight = 1080
+screenWidth = 1080
+screenHeight = 720
 
 
 function love.load()
@@ -55,6 +58,18 @@ function love.update(dt)
         mainMenu.update(dt)
     elseif currentState == "gameover" then
         game.gameOverScreen.update(dt)
+    elseif currentState == "tutorial" then
+        game.tutorial.update(dt)
+        if not game.tutorial.updatePause then
+            game.map.update(dt)
+            game.manager.update(dt)
+            game.creatures.update(dt)
+            game.towerPlacement.update(dt)
+            game.tMenu.update(dt)
+            game.enemyAi.update(dt)
+        end
+
+        game.powerUps.update(dt)
     elseif currentState == "game" then
         game.map.update(dt)
         game.manager.update(dt)
@@ -81,6 +96,18 @@ function love.draw()
 
         game.gameOverScreen.draw()
         mainMenu.changePlayButtonText("Start new Game")
+    elseif currentState == "tutorial" then
+
+        game.map.draw()
+        game.creatures.draw()
+        game.towerPlacement.draw()
+        game.tutorial.draw()
+        game.tMenu.draw()
+        game.enemyAi.draw()
+        game.powerUps.draw()
+        game.manager.draw()
+
+
     elseif currentState == "game" then
 
         game.map.draw()
@@ -100,6 +127,11 @@ function love.mousepressed(x, y, button, istouch, presses)
     elseif currentState == "pause" then
 
         mainMenu.mousepressed(x, y, button, presses)
+    elseif currentState == "tutorial" then
+
+        game.tutorial.mousepressed(x, y, button)
+        --game.map.mousepressed(x, y, button)
+        game.tMenu.mousepressed(x, y, button, istouch, presses)
     elseif currentState == "game" then
         if game.map.editorMode then
             game.map.mousepressed(x, y, button)
@@ -113,6 +145,7 @@ function love.mousepressed(x, y, button, istouch, presses)
 end
 
  function love.keypressed(key)
+     game.tutorial.keypressed(key)
      game.tMenu.keypressed(key)
      if currentState =="gameover" then
          currentState= "menu"
