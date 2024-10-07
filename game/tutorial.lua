b0fcf816f9e086a278991afebde215b2e1078d81
sheaksadi <sheaksadi123456@gmail.com>
2024-10-08 01:50:11 +0600

    big titorial upgrade XD

--- game/tutorial.lua
+++ game/tutorial.lua
@@ -1,45 +1,70 @@
-
 game.tutorial = {}
 
 currentTutorial = 1
-
-
 
 game.tutorial.updatePause = true
 game.tutorial.clickPause = false
 local hasUpgradableTower = false
 local tutorialOver = false
+
+-- Enhanced tutorial message display
+local function drawTutorialPanel(text, subText, hint)
+    -- Draw semi-transparent background panel
+    love.graphics.setColor(0, 0, 0, 0.7)
+    local panelHeight = 180
+    love.graphics.rectangle("fill", 0, screenHeight/8 - 20, screenWidth, panelHeight)
+
+    -- Draw main text
+    love.graphics.setColor(1, 1, 0)
+    drawText(text, nil, screenHeight/8)
+
+    -- Draw sub-text if provided
+    if subText then
+        love.graphics.setColor(0.8, 0.8, 0.8)
+        drawText(subText, nil, screenHeight/8 + 40, 18)
+    end
+
+    -- Draw hint if provided
+    if hint then
+        love.graphics.setColor(0.7, 0.7, 1)
+        drawText(hint, nil, screenHeight/8 + 120, 16)
+    end
+end
+
 tutorialConfig = {
     {
         draw = function()
             drawPlayAreas()
 
-            --love.graphics.print(tutorialText[currentTutorial], screenWidth/2 - font:getWidth(tutorialText[currentTutorial])/2, screenHeight/6)
-            drawText("Left click in the highlighted Blue area to Place a tower to spawn creatures.\nYou cant place towers on top of other towers\n\nCenter of play area is the main hq\nMines generate coin and others spawn creatures")
-            drawText("Red area is enemies area, where enemy places tower", screenWidth/1.5 , screenHeight/1.3)
+            drawTutorialPanel(
+                    "Welcome to the Game!",
+                    "Left click in the highlighted Blue area to Place a tower to spawn creatures.\nYou can't place towers on top of other towers.",
+                    "💡 The center of play area is your main HQ. Mines generate coins and other towers spawn creatures."
+            )
+
+            -- Keep original extra text for red area
+            love.graphics.setColor(1, 0.8, 0.8)
+            drawText("Red area is enemies area, where enemy places tower", screenWidth/1.5, screenHeight/1.3)
         end,
         update = function(dt)
-
             if #game.towerPlacement.towers > 2 then
                 increaseTutorial()
             end
         end,
-        click = function(x, y)
-
-        end,
+        click = function(x, y) end,
         nextFunction = function()
             game.tutorial.updatePause = false
         end
     },
     {
         draw = function()
-            love.graphics.setColor(1, 1, 0)
-            --love.graphics.print(tutorialText[currentTutorial], screenWidth/2 - font:getWidth(tutorialText[currentTutorial])/2, screenHeight/6)
-            drawText("Creatures will spawn from the towers and will attack enemies\n And enemy will also place towers to attack\n\nClick anywhere to continue")
+            drawTutorialPanel(
+                    "Understanding Combat",
+                    "Creatures will spawn from your towers and attack enemies.\nEnemy will also place towers to attack you!",
+                    "💡 Click anywhere to continue"
+            )
         end,
-        update = function(self, dt)
-
-        end,
+        update = function(self, dt) end,
         click = function(self, x, y)
             increaseTutorial()
         end,
@@ -52,14 +77,14 @@ tutorialConfig = {
         end
     },
     {
-
         draw = function()
-
             if not hasUpgradableTower then
-                drawText("Place another tower that's not mine")
+                drawTutorialPanel(
+                        "Building Your Army",
+                        "Place another tower that's not a mine",
+                        "💡 Different tower types spawn different creatures"
+                )
             end
-
-
         end,
         update = function(dt)
             if game.manager.player1.money < 300 then
@@ -69,21 +94,23 @@ tutorialConfig = {
                 for i, v in ipairs(game.towerPlacement.towers) do
                     for i, type in ipairs(game.towerPlacement.towerTypes) do
                         if v.player == 1 then
-                            if type ~= "mine"  and type == v.type then
+                            if type ~= "mine" and type == v.type then
                                 increaseTutorial()
                                 hasUpgradableTower = true
                             end
                         end
-
                     end
                 end
             end
-
         end,
     },
     {
         draw = function()
-            drawText("Left click on the tower to choose creature type \n Right click to Upgrade a tower\n\n\nUpgrade one tower to continue")
+            drawTutorialPanel(
+                    "Tower Management",
+                    "Left click on the tower to choose creature type\nRight click to Upgrade a tower",
+                    "💡 Upgrade one tower to continue"
+            )
         end,
         update = function(dt)
             for i, v in ipairs(game.towerPlacement.towers) do
@@ -95,15 +122,13 @@ tutorialConfig = {
     },
     {
         draw = function()
-            love.graphics.setColor(1, 1, 0)
-            --love.graphics.print(tutorialText[currentTutorial], screenWidth/2 - font:getWidth(tutorialText[currentTutorial])/2, screenHeight/6)
-            drawText("You might have noticed small coins and \nother good things Appear randomly On the map\nClick them to get coins or small powerups")
-            drawText("There are 3 lanes that creatures can take and \nyou cant go through lane walls", nill , screenHeight/2)
-            drawText("Click anywhere continue rest of the game\nI'll give you some money to Dominate this round :\")" , nill , screenHeight/1.4)
+            drawTutorialPanel(
+                    "Final Tips",
+                    "Collect coins and power-ups that appear randomly on the map!\nThere are 3 lanes that creatures can take and you can't cross lane walls.",
+                    "💡 Click anywhere to start playing - I'll give you some money to dominate this round! :)"
+            )
         end,
-        update = function(self, dt)
-
-        end,
+        update = function(self, dt) end,
         click = function(self, x, y)
             increaseTutorial()
         end,
@@ -118,6 +143,7 @@ tutorialConfig = {
     },
 }
 
+-- Keep your original helper functions
 function increaseTutorial(callback)
     if tutorialOver then
         return
@@ -132,8 +158,8 @@ function increaseTutorial(callback)
     end
     currentTutorial = currentTutorial + 1
 end
+
 function drawText(text, x, y, fontSize)
-    love.graphics.setColor(1, 1, 0)
     local newFontS = fontSize or 20
     local font = love.graphics.newFont(newFontS)
     love.graphics.setFont(font)
@@ -143,43 +169,34 @@ function drawText(text, x, y, fontSize)
     end
     love.graphics.printf(
             text,
-             newX,
+            newX,
             y or screenHeight/6,
             font:getWidth(text),
             "center"
     )
 end
 
-
-function game.tutorial.update (dt)
+-- Keep your original main functions
+function game.tutorial.update(dt)
     if tutorialConfig[currentTutorial] then
         if tutorialConfig[currentTutorial].update then
             tutorialConfig[currentTutorial].update(dt)
         end
-    else
-
     end
-
 end
 
-function game.tutorial.draw ()
-
-    --draw text pretty big and center
+function game.tutorial.draw()
     if tutorialConfig[currentTutorial] then
         if tutorialConfig[currentTutorial].draw then
             tutorialConfig[currentTutorial].draw()
         end
     end
+
     if not tutorialOver then
         local font = love.graphics.newFont(14)
-        drawText("Press space bar to go to next step quickly :(", screenWidth - font:getWidth("Press space bar to go to next step quickly :(")/2, 0, 14)
+        love.graphics.setColor(1, 0.7, 1)
+        drawText("Press space bar to skip tutorial step :)", screenWidth - font:getWidth("Press space bar to skip tutorial step :)  ")/2, 10, 14)
     end
-
-
-
-
-
-
 end
 
 function game.tutorial.keypressed(key)
@@ -187,13 +204,11 @@ function game.tutorial.keypressed(key)
         if tutorialConfig[currentTutorial].keypressed then
             tutorialConfig[currentTutorial].keypressed(key)
         end
-
     end
     if key == "space" then
         if not tutorialOver then
             increaseTutorial()
         end
-
     end
 end
 
@@ -203,12 +218,6 @@ function game.tutorial.mousepressed(x, y, button)
             tutorialConfig[currentTutorial].click(x, y)
         end
     end
-
-    --if button == 1 then
-    --    currentTutorial = currentTutorial + 1
-    --    if currentTutorial > #tutorialText then
-    --        currentTutorial = 1
-    --    end
-    --end
 end
 
+return game.tutorial
